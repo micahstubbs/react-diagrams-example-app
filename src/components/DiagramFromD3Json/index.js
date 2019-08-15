742aa2c5d14bac762f2aef51ad04d242b41ee37c
micahstubbs <micah.stubbs@gmail.com>
2019-08-15 15:55:13 -0700

    remove commented code

--- src/components/DiagramFromD3Json/index.js
+++ src/components/DiagramFromD3Json/index.js
@@ -65,23 +65,13 @@ class DemoWidget extends React.Component {
 }
 
 export default () => {
-  //1) setup the diagram engine
+  //! 1) setup the diagram engine
   let engine = createEngine();
 
-  //2) setup the diagram model
+  //! 2) setup the diagram model
   let model = new DiagramModel();
 
-  //3) create a default nodes
-  // let nodesFrom = [];
-  // let nodesTo = [];
-
-  // nodesFrom.push(createNode("from-1"));
-  // nodesFrom.push(createNode("from-2"));
-  // nodesFrom.push(createNode("from-3"));
-
-  // nodesTo.push(createNode("to-1"));
-  // nodesTo.push(createNode("to-2"));
-  // nodesTo.push(createNode("to-3"));
+  //! 3) create a default nodes
 
   // create an array to hold
   // react-diagram's representation of the nodes
@@ -94,10 +84,7 @@ export default () => {
     nodesHash[node.id] = reactDiagramNode;
   });
 
-  //4) link nodes together
-  // let links = nodesFrom.map((node, index) => {
-  //   return connectNodes(node, nodesTo[index], engine, count++);
-  // });
+  //! 4) link nodes together
 
   // create an array to hold
   // react-diagram's represenation of the links
@@ -118,24 +105,6 @@ export default () => {
     });
     links.push(reactDiagramLink);
   });
-
-  // more links for more complicated diagram
-  // links.push(connectNodes(nodesFrom[0], nodesTo[1], engine, count++));
-  // links.push(connectNodes(nodesTo[0], nodesFrom[1], engine, count++));
-  // links.push(connectNodes(nodesFrom[1], nodesTo[2], engine, count++));
-
-  //
-  // set initial position for each node
-  //
-  // nodesFrom.forEach((node, index) => {
-  //   node.setPosition(index * 70, index * 70);
-  //   model.addNode(node);
-  // });
-
-  // nodesTo.forEach((node, index) => {
-  //   node.setPosition(index * 70, 100);
-  //   model.addNode(node);
-  // });
 
   nodes.forEach((node, i) => {
     node.setPosition(i * 70, i * 70);
