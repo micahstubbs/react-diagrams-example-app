192ed8cd46f31f7b6f594b53dd81444099ae2e35
micahstubbs <micah.stubbs@gmail.com>
2019-08-15 11:38:05 -0700

    add more recent example from repo diagrams-demo-gallery dir

--- src/components/Diagram/index.js
+++ src/components/Diagram/index.js
@@ -1,42 +1,47 @@
-import React, { Component } from "react";
-
 import createEngine, {
-  DefaultLinkModel,
-  DefaultNodeModel,
   DiagramModel,
-  CanvasWidget
+  DefaultNodeModel,
+  DefaultLinkModel
 } from "@projectstorm/react-diagrams";
+import * as React from "react";
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
+import { DemoCanvasWidget } from "../DemoCanvasWidget";
 
-class Diagram extends Component {
-  render() {
-    // create an instance of the engine with all the defaults
-    const engine = new createEngine();
+export default () => {
+  //1) setup the diagram engine
+  var engine = createEngine();
 
-    // node 1
-    const node1 = new DefaultNodeModel({
-      name: "Node 1",
-      color: "rgb(0,192,255)"
-    });
-    node1.setPosition(100, 100);
-    let port1 = node1.addOutPort("Out");
+  //2) setup the diagram model
+  var model = new DiagramModel();
 
-    // node 2
-    const node2 = new DefaultNodeModel({
-      name: "Node 1",
-      color: "rgb(0,192,255)"
-    });
-    node2.setPosition(100, 100);
-    let port2 = node2.addOutPort("Out");
+  //3-A) create a default node
+  var node1 = new DefaultNodeModel({
+    name: "Node 1",
+    color: "rgb(0,192,255)"
+  });
+  node1.setPosition(100, 100);
+  let port1 = node1.addOutPort("Out");
 
-    // link them and add a label to the link
-    const link = port1.link < DefaultLinkModel > port2;
-    link.addLabel("Hello World!");
+  //3-B) create another default node
+  var node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
+  let port2 = node2.addInPort("In");
+  node2.setPosition(400, 100);
 
-    const model = new DiagramModel();
-    model.addAll(node1, node2, link);
-    engine.setModel(model);
-    return <CanvasWidget engine={engine} />;
-  }
-}
+  // link the ports
+  let link1 = port1.link(port2);
+  link1.getOptions().testName = "Test";
+  link1.addLabel("Hello World!");
 
-export default Diagram;
+  //4) add the models to the root graph
+  model.addAll(node1, node2, link1);
+
+  //5) load model into engine
+  engine.setModel(model);
+
+  //6) render the diagram!
+  return (
+    <DemoCanvasWidget>
+      <CanvasWidget engine={engine} />
+    </DemoCanvasWidget>
+  );
+};
