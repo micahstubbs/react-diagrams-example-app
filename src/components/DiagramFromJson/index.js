12744796b836178f812534017395bace8bc927b6
micahstubbs <micah.stubbs@gmail.com>
2019-08-15 13:06:52 -0700

    remove commented code and simplify DiagramFromJson example

--- src/components/DiagramFromJson/index.js
+++ src/components/DiagramFromJson/index.js
@@ -1,65 +1,24 @@
-import createEngine, {
-  DiagramModel,
-  DefaultNodeModel
-} from "@projectstorm/react-diagrams";
+import createEngine, { DiagramModel } from "@projectstorm/react-diagrams";
 import * as React from "react";
-import DemoWorkspaceWidget, { DemoButton } from "../DemoWorkspaceWidget";
-import * as beautify from "json-beautify";
+import DemoWorkspaceWidget from "../DemoWorkspaceWidget";
 import { CanvasWidget } from "@projectstorm/react-canvas-core";
 import { DemoCanvasWidget } from "../DemoCanvasWidget";
 
 import jsonGraph from "../../data/two-node-graph.json";
 
 export default () => {
-  //1) setup the diagram engine
+  // setup the diagram engine
   var engine = createEngine();
 
-  // //2) setup the diagram model
-  // var model = new DiagramModel();
+  //!------------- DESERIALIZE ----------------
 
-  // //3-A) create a default node
-  // var node1 = new DefaultNodeModel("Node 1", "rgb(0,192,255)");
-  // var port1 = node1.addOutPort("Out");
-  // node1.setPosition(100, 100);
-
-  // //3-B) create another default node
-  // var node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
-  // var port2 = node2.addInPort("In");
-  // node2.setPosition(400, 100);
-
-  // //3-C) link the 2 nodes together
-  // var link1 = port1.link(port2);
-
-  // //4) add the models to the root graph
-  // model.addAll(node1, node2, link1);
-
-  // //5) load model into engine
-  // engine.setModel(model);
-
-  // //!------------- SERIALIZING ------------------
-
-  // const graph = model.serialize();
-  // console.log("graph", graph);
-  // var str = JSON.stringify(graph);
-
-  //!------------- DESERIALIZING ----------------
-
-  var model2 = new DiagramModel();
+  var model = new DiagramModel();
   console.log("jsonGraph", jsonGraph);
-  model2.deserializeModel(jsonGraph, engine);
-  engine.setModel(model2);
-
-  // const onClick = () => {
-  //   beautify(model2.serialize(), null, 2, 80);
-  //   // action("Serialized Graph")(
-  //   //   beautify(model2.serialize(), null, 2, 80)
-  //   // );
-  // };
+  model.deserializeModel(jsonGraph, engine);
+  engine.setModel(model);
 
   return (
-    <DemoWorkspaceWidget
-    // buttons={<DemoButton onClick={onClick}>Serialize Graph</DemoButton>}
-    >
+    <DemoWorkspaceWidget>
       <DemoCanvasWidget>
         <CanvasWidget engine={engine} />
       </DemoCanvasWidget>
